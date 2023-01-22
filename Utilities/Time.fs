bf40a456d695310770d8dee006239b8cb65d6736
Nicholas Bannayi <nbannayi@hotmail.co.uk>
2023-01-22 00:03:31 +0000

    Update solution files

--- Utilities/Time.fs
+++ Utilities/Time.fs
@@ -1,1 +1,11 @@
-﻿
+﻿namespace AoC2022.Utilities
+
+open System.Diagnostics
+      
+module Time =
+
+    let displayElapsed (sw: Stopwatch) =
+        printfn "Took %.1f minutes, (%.2fs, %dms)"
+            (double sw.ElapsedMilliseconds / 60000.0)
+            (double sw.ElapsedMilliseconds/1000.)
+            sw.ElapsedMilliseconds
